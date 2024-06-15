b3b771e7603f20805b97686ca664232a49f40bde
shahriar2880 <shahriarreza436@gmail.com>
2024-06-15 23:38:36 +0600

    break_with_while_loop.js

--- break_with_while_loop.js
+++ break_with_while_loop.js
@@ -1,9 +1,18 @@
-var orderQuantity = 0;
-var freeQuantity = 0;
-while(orderQuantity <=144){
-    freeQuantity++
-    console.log("free add hobe ")
-    if(freeQuantity >= 12){
+// var orderQuantity = 0;
+// var freeQuantity = 0;
+// while(orderQuantity <=144){
+//     freeQuantity++
+//     console.log("free add hobe ")
+//     if(freeQuantity >= 12){
+//         break;
+//     }
+// }
+
+var numbers = [44,45,46,47,48,49,50,122,24,48,49,50];
+for(var i=0; i<7; i++){
+    var number = numbers[i];
+    if(numbers >= 100){
         break;
     }
+    console.log(number);
 }
