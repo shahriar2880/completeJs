c0c5c30a1e2352e42e6d57db8d51a15b9c1a3e86
shahriar2880 <shahriarreza436@gmail.com>
2024-06-15 23:34:47 +0600

    break_with_while_loop.js

---
+++ break_with_while_loop.js
@@ -0,0 +1,9 @@
+var orderQuantity = 0;
+var freeQuantity = 0;
+while(orderQuantity <=144){
+    freeQuantity++
+    console.log("free add hobe ")
+    if(freeQuantity >= 12){
+        break;
+    }
+}
