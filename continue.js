c4c005d52897addeb6fa59a238baea7d04ea2c64
shahriar2880 <shahriarreza436@gmail.com>
2024-06-17 01:05:09 +0600

    continue

--- continue.js
+++ continue.js
@@ -1,8 +1,8 @@
-var numbers = [44,45,46,47,48,49,50,122,24,48,49,50,8,49,50,122,24,48,49,50];
-for(var i=0; i<numbers.length; i++){
-    var number = numbers[i];
-    if(number > 50){
-        continue; 
-    }
-    console.log(number);
-}
+// var numbers = [44,45,46,47,48,49,50,122,24,48,49,50,8,49,50,122,24,48,49,50];
+// for(var i=0; i<numbers.length; i++){
+//     var number = numbers[i];
+//     if(number > 50){
+//         continue; 
+//     }
+//     console.log(number);
+// }
