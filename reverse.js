79424e73e04d255bbce446ccafee86f112bde0a9
shahriar2880 <shahriarreza436@gmail.com>
2024-06-17 16:09:01 +0600

    reverse using for and while loop

---
+++ reverse.js
@@ -0,0 +1,25 @@
+//------print 1 to 10 using while loop-----// 
+var num =1;
+while(num <= 10){
+    console.log(num);
+    num++;
+}
+
+//------print 10 to 1 using while loop-----// 
+
+var num =10;
+while(num >= 1){
+    console.log(num);
+    num--;
+}
+
+//------print 1 to 10 using for loop-----// 
+
+for(i=1; i<= 10;i++){
+    console.log(i)
+}
+
+//------print 10 to 1 using for loop-----// 
+for(i=10; i>= 1;i--){
+    console.log(i)
+}
